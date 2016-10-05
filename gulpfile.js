a01c8e633c63ed415c3c22925cce69854d9bfa68
Simon <simonxd@gmail.com>
2016-10-05 14:35:09 -0300

    removed client

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,11 @@
 const jwt         = require('jsonwebtoken');
 const MongoClient = require('mongodb').MongoClient;
 const gulp        = require('gulp');
-const runSequence = require('run-sequence');
 const gulpNodemon = require('gulp-nodemon');
 
 // tests
 const istanbul    = require('gulp-istanbul');
 const mocha       = require('gulp-mocha');
-
-// browserSync
-const browserSync = require('browser-sync').create();
 
 const DEV_DB_URI = 'mongodb://localhost:27017/h-auth-development-db';
 const DEV_RABBIT_MQ_URI = 'amqp://192.168.99.100';
@@ -86,78 +82,4 @@ gulp.task('test', ['pre-test'], function () {
     .on('error', (err) => {
       this.emit('error', err);
     });
-});
-
-/**
- * Testing through controllers
- */
-gulp.task('pre-test-controllers', function () {
-  return gulp.src(['server/**/*.js', 'shared/**/*.js'])
-    // Covering files
-    .pipe(istanbul())
-    // Force `require` to return covered files
-    .pipe(istanbul.hookRequire());
-});
-
-gulp.task('test-controllers', ['pre-test-controllers'], function () {
-  return gulp.src(['test/tests/controllers/**/*.js'])
-    .pipe(mocha())
-    // Creating the reports after tests ran
-    .pipe(istanbul.writeReports())
-    // Enforce a coverage of at least 90%
-    .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }))
-    .on('error', (err) => {
-      this.emit('error', err);
-    });
-});
-
-
-// MOCK SERVER
-gulp.task('mock', function () {
-  gulpNodemon({
-    script: 'mock/cli.js',
-    ext: 'js',
-    ignore: [
-      'client/**/*',
-      'dist/**/*',
-      'gulpfile.js',
-    ],
-  });
-});
-
-// CLIENT //
-require('./tasks/client/build')(gulp);
-// Static server
-gulp.task('serve:client', function() {
-  browserSync.init({
-    server: {
-      baseDir: ['./demo', './dist', './client']
-    },
-    open: true
-  });
-});
-
-/**
- * Watch for changes and auto recompile
- */
-gulp.task('watch', function () {
-
-  var clientFiles = [
-    'client/**/*.js',
-    'client/**/*.html',
-    'client/**/*.css',
-  ];
-
-  gulp.watch(clientFiles, ['javascript']);
-  gulp.watch([
-    'dist/**/*.js',
-    'demo/**/*'
-  ], browserSync.reload);
-});
-
-/**
- * Main development task
- */
-gulp.task('develop', function () {
-  runSequence('javascript', 'serve:client', 'watch', 'nodemon');
 });
