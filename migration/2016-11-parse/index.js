ee5b212f21090c43efe65a110f6cf618e46b3609
Simon <simonxd@gmail.com>
2016-11-04 00:02:51 -0200

    initial

---
+++ migration/2016-11-parse/index.js
@@ -0,0 +1,55 @@
+// third-party
+const Bluebird = require('bluebird');
+
+const CONSTANTS = require('../../shared/constants');
+
+const usersData = require('./raw-data/_User.json').results;
+
+module.exports = function (hAccount) {
+
+  return hAccount.ready.then(() => {
+    console.log('hAccount ready');
+
+    const AccountLock = hAccount.services.accountLock.models.Lock;
+    const Account     = hAccount.services.mongoose.models.Account;
+
+    return usersData.reduce((lastPromise, userData) => {
+
+      return lastPromise.then(() => {
+
+        var lock = new AccountLock({
+          _hash: userData.bcryptPassword,
+        });
+
+        return lock.save();
+      })
+      .then((lock) => {
+
+        var account = new Account({
+          name: userData.name,
+          createdAt: userData.createdAt,
+          verifiedAt: userData.updatedAt,
+          username: userData.username,
+          email: userData.email || userData.username,
+          _accLockId: lock._id,
+        });
+
+        account.setStatus(
+          CONSTANTS.ACCOUNT_STATUSES.VERIFIED,
+          'MigratedFromParse'
+        );
+
+        return account.save();
+      });
+
+    }, Bluebird.resolve());
+
+  })
+  .then(() => {
+    console.log('migration succeeded');
+  })
+  .catch((err) => {
+    console.warn('error', err);
+  });
+
+};
