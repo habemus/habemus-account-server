cba7527a066e90d3949249a99ca1e643d3560886
Simon <simonxd@gmail.com>
2016-11-15 21:16:02 -0200

    exposing email in public api

--- server/routes/interfaces.js
+++ server/routes/interfaces.js
@@ -7,9 +7,16 @@ exports.TOKEN_DATA = {
   exp: true,
 };
 
+/**
+ * Currently used for exposing data for the account
+ * to the account owner.
+ * May require modfications for total public usage (e.g. remove email)
+ * 
+ * @type {Object}
+ */
 exports.ACCOUNT_DATA = {
   username: true,
-  // email: true,
+  email: true,
   createdAt: true,
   'status.value': true,
   'status.reason': true,
